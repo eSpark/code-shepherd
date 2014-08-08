35861755d2d0613d742ce3fd2ce0b357cb7d8744
Alex Koppel <alex@alexkoppel.com>
2014-08-08 16:09:43 -0500

    Add spec

--- spec/lib/pull_request/reviewers_spec.rb
+++ spec/lib/pull_request/reviewers_spec.rb
@@ -11,6 +11,12 @@ module Shepherd
           reviewers = Reviewers.extract_from_text(text)
           expect(reviewers.reviewers).to eq(["abc", "def"])
         end
+
+        it "properly parses commit messages w/o reviewers" do
+          text = "Deleting and Adding work, no lookahead support yet. \r\n\r\nDeleting is instantaneous for now.  A safe guard is probably a good idea to implement in case accidental deletions happen. \r\n\r\nAdding stuff is done by typing the IDs directly into the textbox.  Attach \"s\" to the beginning of an ID to represent a section. \r\nEX: '1,s2,3' will add students with ID 1 and 3, and section with ID 2.  \r\nThis was made with the assumption that a lookahead will be used with it without allowing users to edit the box directly.  As it is now it presents obvious security issues.  \r\n\r\nNo major stylistic modifications have been made yet.  "
+          reviewers = Reviewers.extract_from_text(text)
+          expect(reviewers.reviewers).to eq([])
+        end
       end
 
       describe "#valid?" do
